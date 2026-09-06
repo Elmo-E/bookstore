47d597ad712e5d90a91eb015cb61eefe3bff9019
Elmo Eklund <elmo.eklund@gmail.com>
2026-09-06 21:02:43 +0300

    Lisätty booklist

--- src/main/java/bookstore/bookstore/web/BookController.java
+++ src/main/java/bookstore/bookstore/web/BookController.java
@@ -3,12 +3,27 @@ package bookstore.bookstore.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import bookstore.bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
+
+    private final BookRepository repository;
+
+    public BookController(BookRepository repository) {
+        this.repository = repository;
+    }
+
     @GetMapping("/index")
     @ResponseBody
     public String index() {
         return "This is the bookstore index page";
+    }
+
+    @GetMapping("/booklist")
+    public String bookList(Model model) {
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
     }
 }

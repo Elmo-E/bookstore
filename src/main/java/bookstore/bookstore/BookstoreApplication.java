27041afd72ad37d9e48c23ad24a60db0b45c7d2e
Elmo Eklund <elmo.eklund@gmail.com>
2026-09-06 20:54:57 +0300

    Lisätty JPA, H2 database ja demo data

--- src/main/java/bookstore/bookstore/BookstoreApplication.java
+++ src/main/java/bookstore/bookstore/BookstoreApplication.java
@@ -1,7 +1,12 @@
 package bookstore.bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import bookstore.bookstore.domain.Book;
+import bookstore.bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -10,4 +15,11 @@ public class BookstoreApplication {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner demoData(BookRepository repository) {
+		return (args) -> {
+			repository.save(new Book("A Game of Thrones", "George R. R. Martin", 1996, "978-0553103540", 12.90));
+			repository.save(new Book("Dune", "Frank Herbert", 1965, "978-0441172719", 9.50));
+		};
+	}
 }
--- src/main/java/bookstore/bookstore/domain/Book.java
+++ src/main/java/bookstore/bookstore/domain/Book.java
@@ -1,6 +1,17 @@
 package bookstore.bookstore.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     private String title;
     private String author;
@@ -57,6 +68,14 @@ public class Book {
 
     public void setPrice(double price) {
         this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
 }

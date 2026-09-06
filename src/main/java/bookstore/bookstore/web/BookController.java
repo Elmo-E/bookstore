cf2c1230369915ed3bf0ac4080f003dca058432e
Elmo Eklund <elmo.eklund@gmail.com>
2026-09-06 20:09:45 +0300

    Lisätty BookController ja Book luokat

---
+++ src/main/java/bookstore/bookstore/web/BookController.java
@@ -0,0 +1,14 @@
+package bookstore.bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BookController {
+    @GetMapping("/index")
+    @ResponseBody
+    public String index() {
+        return "This is the bookstore index page";
+    }
+}

0a3202927ac98cd73657eca9c01e4613d13e3212
Elmo Eklund <elmo.eklund@gmail.com>
2026-09-06 21:47:55 +0300

    Lisätty editointi

--- src/main/java/bookstore/bookstore/web/BookController.java
+++ src/main/java/bookstore/bookstore/web/BookController.java
@@ -1,9 +1,13 @@
 package bookstore.bookstore.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import bookstore.bookstore.domain.Book;
 import bookstore.bookstore.domain.BookRepository;
 
 @Controller
@@ -26,4 +30,33 @@ public class BookController {
         model.addAttribute("books", repository.findAll());
         return "booklist";
     }
+
+    // Näytä lomake uuden kirjan lisäämiseen
+    @GetMapping("/add")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    // Tallenna lomakkeelta tullut kirja
+    @PostMapping("/save")
+    public String save(Book book) {
+        repository.save(book);
+        return "redirect:/booklist";
+    }
+
+    // Poista kirja id:n perusteella
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long bookId) {
+        repository.deleteById(bookId);
+        return "redirect:/booklist";
+    }
+
+    // Näytä muokkauslomake nykyisillä tiedoilla
+    @GetMapping("/edit/{id}")
+    public String editBook(@PathVariable("id") Long bookId, Model model) {
+        model.addAttribute("book", repository.findById(bookId).get());
+        return "editbook";
+    }
+
 }
